554b45ec0b4730b4cd3c45e6a8bf49525ac204ac
Kadem Durga Prasad <kademkalyan004@gmail.com>
2025-01-14 22:47:05 +0530

    Update Arrays-XV.cpp

--- Arrays-XV/Arrays-XV.cpp
+++ Arrays-XV/Arrays-XV.cpp
@@ -20,3 +20,25 @@ int subarraywithXor(vecto<int> a, int k){
 }
 
 //Time Complexity: O(N3
+
+// better approach
+int subarraysWithXorK(vector<int> a, int k) {
+    int n = a.size(); //size of the given array.
+    int cnt = 0;
+
+    // Step 1: Generating subarrays:
+    for (int i = 0; i < n; i++) {
+        int xorr = 0;
+        for (int j = i; j < n; j++) {
+
+            //step 2:calculate XOR of all
+            // elements:
+            xorr = xorr ^ a[j];
+
+            // step 3:check XOR and count:
+            if (xorr == k) cnt++;
+        }
+    }
+    return cnt;
+}
+//Time Complexity: O(N2)
