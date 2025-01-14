d6eb4198a1b8da79ca3e09f1358ea441e5f04832
Kadem Durga Prasad <kademkalyan004@gmail.com>
2025-01-14 22:48:35 +0530

    Update Arrays-XV.cpp

--- Arrays-XV/Arrays-XV.cpp
+++ Arrays-XV/Arrays-XV.cpp
@@ -75,3 +75,38 @@ int main()
          << ans << "\n";
     return 0;
 }
+
+//Merge Overlapping Sub-intervals
+// brute force
+vector<vector<int>> mergeOverlapping(vector<vector<int>> &arr){
+    int n = arr.size();
+
+    // sort the intervals
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> ans;
+
+    for(int i = 0; i < n; i++){
+        int start = arr[i][0];
+        int end = arr[i][1];
+
+        // skip all the merged intervals
+        if(!ans.empty() && end <= ans.back()[1]){
+            continue;
+        }
+
+        // check fir the rest of the intervals
+        for(int j = i + 1; j < n; j++){
+            if(arr[j][0] <= end){
+                end = max(end, arr[j][1]);
+            }
+            else{
+                break;
+            }
+        }
+        ans.push_back({stard, end});
+    }
+    return ans;
+}
+
+//Time Complexity: O(N*logN) + O(2*N)
