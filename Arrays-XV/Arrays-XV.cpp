f7dd9087d1bc53e8f9cd1fecf8e4be215f381aa4
Kadem Durga Prasad <kademkalyan004@gmail.com>
2025-01-14 22:46:52 +0530

    Count subarrays with sum k with xor brute force

--- Arrays-XV/Arrays-XV.cpp
+++ Arrays-XV/Arrays-XV.cpp
@@ -1,2 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// count subarrays with sum k with xor
+int subarraywithXor(vecto<int> a, int k){
+    int n = a.size();
+    int cnt = 0;
+
+    // generating subarrays
+    for(int i = 0; i < n; i++){
+        for(int j  = i; j < n; j++){
+            int xorr= 0;
+            for(int k = i; k <= j; k++){
+                xorr = xorr ^ a[k];
+            }
+            if(xorr == k) cnt++;
+        }
+    }
+    return cnt;
+}
+
+//Time Complexity: O(N3
